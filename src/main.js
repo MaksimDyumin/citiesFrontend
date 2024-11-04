f38f43a9a7ba5feb123f8981b77dbdb9de827f9a
Maxim Dyumin <maxdyumin2000@yandex.ru>
2024-11-04 20:09:20 +0300

    import he-tree css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import '@he-tree/vue/style/default.css'
+import '@he-tree/vue/style/material-design.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
